c2d9470420d30a09c9b21536f82d0dbd25c870b5
Kevin <kevin.freistroffer@gmail.com>
2019-05-08 13:05:09 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scrape';
+  urls: String[] = [
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com',
+  	'google.com'
+  ];
+
+  formIsValid: boolean = false;
+
+  constructor() {
+
+  }
+
+  handleOnSubmit() {
+  	// If no inputs have a value 
+  	// this.formIsValid = false
+  	// else this.formIsValid = true
+
+  	if (this.formIsValid) {
+
+  	}
+  }
+
+  handleOnChange(event) {
+
+  }
 }
